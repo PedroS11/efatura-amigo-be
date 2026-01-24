a21bc776ad8089b5a417b018c4930712cf9f41d9
Pedro Oliveira da Silva <9307951+PedroS11@users.noreply.github.com>
2026-01-24 17:27:15 +0000

    Add API

--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from "aws-cdk-lib";
-import { AttributeType, TableV2 } from "aws-cdk-lib/aws-dynamodb";
+import { Duration } from "aws-cdk-lib";
+import { HttpApi, HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import { AttributeType, Billing, TableV2 } from "aws-cdk-lib/aws-dynamodb";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Queue } from "aws-cdk-lib/aws-sqs";
 import type { Construct } from "constructs";
@@ -8,12 +11,15 @@ export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const processNifDLQ = new Queue(this, "ProcessNifDLQ");
+    const processNifDLQ = new Queue(this, "ProcessNifDLQ", {
+      retentionPeriod: cdk.Duration.days(1)
+    });
     const processNifSQS = new Queue(this, "ProcessNifSQS", {
       deadLetterQueue: {
         queue: processNifDLQ,
         maxReceiveCount: 2
-      }
+      },
+      retentionPeriod: cdk.Duration.days(1)
     });
 
     const table = new TableV2(this, "NifCategoryTable", {
@@ -24,13 +30,16 @@ export class Stack extends cdk.Stack {
       sortKey: {
         type: AttributeType.NUMBER,
         name: "category"
-      }
+      },
+      billing: Billing.onDemand()
     });
 
     const getCategoryLambda = new Function(this, "GetCategory", {
       runtime: Runtime.NODEJS_22_X,
       handler: "index.handler",
-      code: Code.fromAsset("dist/getCategory")
+      code: Code.fromAsset("dist/getCategory"),
+      memorySize: 128,
+      reservedConcurrentExecutions: 2
     });
 
     table.grantReadData(getCategoryLambda);
@@ -42,7 +51,10 @@ export class Stack extends cdk.Stack {
     const processNifLambda = new Function(this, "ProcessNif", {
       runtime: Runtime.NODEJS_22_X,
       handler: "index.handler",
-      code: Code.fromAsset("dist/processNif")
+      code: Code.fromAsset("dist/processNif"),
+      memorySize: 128,
+      reservedConcurrentExecutions: 1,
+      timeout: Duration.seconds(10)
     });
 
     table.grantWriteData(processNifLambda);
@@ -50,5 +62,20 @@ export class Stack extends cdk.Stack {
 
     processNifSQS.grantConsumeMessages(processNifLambda);
     processNifLambda.addEnvironment("PROCESS_NIF_SQS", processNifSQS.queueName);
+
+    const httpApi = new HttpApi(this, "EfaturaAmigoApi", {
+      apiName: "EfaturaAmigoApi",
+      createDefaultStage: true
+    });
+
+    httpApi.addRoutes({
+      path: "/category/:nif",
+      methods: [HttpMethod.GET],
+      integration: new HttpLambdaIntegration("LambdaIntegration", getCategoryLambda)
+    });
+
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: httpApi.url!
+    });
   }
 }
