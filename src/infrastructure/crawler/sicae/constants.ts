b5452eee74c64a965998b01f84c5906c97da1130
Pedro Oliveira da Silva <9307951+PedroS11@users.noreply.github.com>
2026-01-29 22:52:01 +0000

    Remove chromium layer + crawlers code

--- src/infrastructure/crawler/sicae/constants.ts
+++
@@ -1,5 +0,0 @@
-export const NIF_INPUT_ID = "#ctl00_MainContent_ipNipc";
-export const SEARCH_BUTTON_ID = "#ctl00_MainContent_btnPesquisa";
-export const SICAE_URL = "http://www.sicae.pt/Consulta.aspx";
-export const INVALID_NIF_MESSAGE_ID = "#ctl00_MainContent_lblError";
-export const VALID_NIF_TABLE_ID = 'td:has-text("CAE Principal")';
--- src/infrastructure/crawler/sicae/types.ts
+++
@@ -1,6 +0,0 @@
-export interface SiCAEData {
-  nif: number;
-  name: string;
-  cae: number;
-  cae2: number[];
-}
