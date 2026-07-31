d8cc5f2d0a27cd0a87b7b30aa7a034c812cc65f6
Pedro Oliveira da Silva <9307951+PedroS11@users.noreply.github.com>
2026-07-31 15:14:25 +0100

    Test feature-branches deploys

--- cdk/index.ts
+++ cdk/index.ts
@@ -4,7 +4,17 @@ import * as cdk from "aws-cdk-lib";
 import { Stack } from "./stack";
 
 const app = new cdk.App();
-new Stack(app, "EfaturaAmigoBeStack", {
+
+const getStackName = () => {
+  let stackname = "EfaturaAmigoBeStack";
+  if (process.env.DEPLOY_ENV) {
+    stackname += `--${process.env.DEPLOY_ENV}`;
+  }
+
+  return stackname;
+};
+
+new Stack(app, getStackName(), {
   env: {
     region: "eu-west-2",
     account: "566348719618"
