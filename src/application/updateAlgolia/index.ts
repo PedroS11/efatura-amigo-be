916d7610efb8444daf23c74b7700897962f31676
Pedro Oliveira da Silva <9307951+PedroS11@users.noreply.github.com>
2026-09-25 13:17:26 +0100

    log event

--- src/application/updateAlgolia/index.ts
+++ src/application/updateAlgolia/index.ts
@@ -1,8 +1,10 @@
 import type { DynamoDBStreamEvent } from "aws-lambda";
 import { saveCompanyInAlgolia } from "../../infrastructure/companiesIndex";
 import type { Company } from "../../infrastructure/companiesTable/types";
+import { logMessage } from "../../infrastructure/utils/logger";
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
+  logMessage("event", JSON.stringify(event));
   for (const record of event.Records) {
     if (["INSERT", "MODIFY"].includes(record.eventName!)) {
       const company: Company = record.dynamodb!.NewImage! as unknown as Company;
