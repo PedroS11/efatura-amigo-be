2743d0b87f0e4205fa78f86e0350ea53d92a9977
Pedro Oliveira da Silva <9307951+PedroS11@users.noreply.github.com>
2026-09-25 13:43:14 +0100

    Fix types on record

--- src/application/updateAlgolia/index.ts
+++ src/application/updateAlgolia/index.ts
@@ -1,6 +1,7 @@
 import type { DynamoDBRecord, SQSEvent } from "aws-lambda";
 import { saveCompanyInAlgolia } from "../../infrastructure/companiesIndex";
 import type { Company } from "../../infrastructure/companiesTable/types";
+import { unmarshallRecord } from "../../infrastructure/utils/aws/dynamo/utils";
 import { logMessage } from "../../infrastructure/utils/logger";
 
 export const handler = async (event: SQSEvent): Promise<void> => {
@@ -8,7 +9,7 @@ export const handler = async (event: SQSEvent): Promise<void> => {
   for (const record of event.Records) {
     const dynamoDbStream: DynamoDBRecord = JSON.parse(record.body);
     if (["INSERT", "MODIFY"].includes(dynamoDbStream.eventName!)) {
-      const company: Company = dynamoDbStream.dynamodb!.NewImage! as unknown as Company;
+      const company: Company = unmarshallRecord<Company>(dynamoDbStream.dynamodb?.NewImage);
 
       await saveCompanyInAlgolia(company);
     }
